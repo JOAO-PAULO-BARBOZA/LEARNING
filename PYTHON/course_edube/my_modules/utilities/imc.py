01a1f8b8cbe8b4277d0b735ac76d4dbcabe3985d
João_Paulo <jpbbarboza1987@gmail.com>
2021-10-11 13:39:19 -0300

    adição do programa seven segment display

--- PYTHON/course_edube/my_modules/utilities/imc.py
+++ PYTHON/course_edube/my_modules/utilities/imc.py
@@ -1,5 +1,12 @@
 """Calcula o imc de uma pessoa"""
+import sys
 
 
 def imc(altura, massa):
     return massa / altura ** 2
+
+
+
+if __name__ == "__main__":
+    print("Don't do that!")
+    sys.exit()
